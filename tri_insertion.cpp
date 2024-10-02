d831256e69e89d73b6d593357c7c1675cdc87d77
OumaymaBrd <oumaymabramid@gmail.com>
2024-10-02 10:05:18 +0100

    tri par insertion

---
+++ tri_insertion.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+	printf("\t\t Tri Par insertion : Tri croissante\n\n");
+	
+	int T[11]={8,9,12,1,3,2,5,4,0,10,11};
+	
+	//Avant le TRi 
+	int n=11;
+	printf("Tableau Avant le Tri: ");
+	for(int i=0;i<n;i++) printf("%d | ",T[i]);
+	
+	//Tri : algorithme de tri par insertion
+	
+	for(int i=1;i<n;i++){
+		
+		int tmp=T[i];
+		int j=i-1;
+		while(T[j]>tmp && j>=0){
+			T[j+1]=T[j];
+			j--;
+		}
+		T[j+1]=tmp;
+		
+	}
+	
+	//affichage apres Tri 
+	printf("\n\nTableau Apres le Tri: ");  for(int i=0;i<n;i++) printf("%d | ",T[i]);
+}
